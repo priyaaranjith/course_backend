581f000859adafc8ed0be21d676ae68fb8b97abc
priyaaranjith <ranjithpriya307@gmail.com>
2022-12-06 11:08:35 +0530

    add

--- src/main/java/com/example/CourseApp_backend/controller/CourseController.java
+++ src/main/java/com/example/CourseApp_backend/controller/CourseController.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class CourseController {
 
+    @PostMapping("/add")
+    public String AddCourse(){
+        return "courses added";
+    }
+
+
 
 
 }
